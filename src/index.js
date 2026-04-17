ae3333b19298eb9334059443cf41ed5199ab42e0
Mike <mmrrff2099@gmail.com>
2026-04-17 17:31:36 +0300

    Third task

--- src/index.js
+++ src/index.js
@@ -3,41 +3,41 @@ function createLogger(prefix) {
     return function strLogger(message) {
         console.log(`${prefix}: ${message}`);
     };
-};
+}
 
-const authLogger = createLogger('AUTH');
+const authLogger = createLogger("AUTH");
 
-const apiLogger = createLogger('API');
+const apiLogger = createLogger("API");
 
-authLogger('User logged in');
+authLogger("User logged in");
 
-apiLogger('Request failed');
+apiLogger("Request failed");
 
 // 2
 function createLimiter(limit) {
-    if(typeof limit !== 'number' || Number.isNaN(limit)) {
-        return function() {
-            return 'Enter a number, please.';
-        }
-    };
+    if (typeof limit !== "number" || Number.isNaN(limit)) {
+        return function () {
+            return "Enter a number, please.";
+        };
+    }
 
-    if(limit < 0) {
-        return function() {
-            return 'Enter a positive number.'
-        }
-    };
+    if (limit < 0) {
+        return function () {
+            return "Enter a positive number.";
+        };
+    }
 
     let count = 0;
 
-    return function() {
+    return function () {
         if (count < limit) {
             count++;
-            return 'Ok';
+            return "Ok";
         } else {
-            return 'Error';
+            return "Error";
         }
     };
-};
+}
 
 const limited = createLimiter(2);
 
@@ -46,3 +46,19 @@ console.log(limited());
 console.log(limited());
 
 console.log(limited());
+
+// 3
+function createFilter(callbackfn) {
+    return function (array) {
+        return array.filter(callbackfn);
+    };
+}
+
+const arrNumb1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrNumb2 = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const getEvenNumbers = createFilter((el) => el % 2 === 0);
+console.log(getEvenNumbers(arrNumb1));
+
+const getGreateTenNumbers = createFilter((el) => el > 10);
+console.log(getGreateTenNumbers(arrNumb2));
